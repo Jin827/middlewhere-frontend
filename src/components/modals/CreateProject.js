b6c0487af631bf3af0120d0e9b41719d3aa82500
candicecz <czech.candice@gmail.com>
2017-08-17 15:53:43 -0400

    resizing projects in homepage

--- src/components/modals/CreateProject.js
+++ src/components/modals/CreateProject.js
@@ -4,6 +4,7 @@ import api from '../../api'
 import {browserHistory as history} from 'react-router';
 import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton';
+import DatePicker from 'material-ui/DatePicker';
 
 export default class CreateProject extends Component {
   constructor(props) {
@@ -25,12 +26,20 @@ export default class CreateProject extends Component {
     this._fetchData()
     }
 
+  _handleChange = (e, date) => {
+    this.setState ({
+      date:date
+    })
+}
+
+
 //put required title
   _fetchData = () =>{
-    api.createProjects(this.refs.title.getValue(), this.refs.deadline.getValue(), this.refs.description.getValue(), localStorage.token)
+    console.log(this.state.date, "val")
+    api.createProjects(this.refs.title.getValue(), this.refs.deadline.value, this.refs.description.getValue(), localStorage.token)
     .then (res => {
       var solving = JSON.parse(res.text)
-      console.log(solving[0].id)
+      // console.log(solving[0].id)
       history.push(`/projects/${solving[0].id}`)
     })
 
@@ -40,7 +49,7 @@ export default class CreateProject extends Component {
     return (
       <div className="createNewProject">
           <TextField floatingLabelText="Title: " type="text" ref="title" maxLength='100'/>
-          <TextField floatingLabelText="Deadline: " type="text" ref="deadline" maxLength='100'/>
+          <DatePicker hintText="Deadline" mode="landscape" ref="deadline" onChange={e => this._handleChange(e)} value={this.state.date}/>
           <TextField floatingLabelText="Description: " type="text" ref="description" maxLength="500" onInput={e => this.handleInput(e)} value={this.state.inputValue}/>
           {140 - this.state.inputValue.length}
           <FlatButton label="Submit" secondary={true} onClick={(e) => this._handleClick(e)} />
