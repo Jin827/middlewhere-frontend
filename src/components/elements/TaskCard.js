9b843978bb56f0a9f176b894318590f6a5544519
rckmnz <rckmnz05@gmail.com>
2017-08-21 12:37:39 -0400

    tweaks

--- src/components/elements/TaskCard.js
+++ src/components/elements/TaskCard.js
@@ -131,7 +131,7 @@ export default class TaskCard extends Component {
             <Card className="task-card">
                 <CardTitle title={ title }  actAsExpander={true} showExpandableButton={true}/>
                 {deadline ? <CardText expandable={true}> <strong>Deadline </strong> <br/> { time } </CardText> : null}
-                <CardText expandable={true}> <strong>Description </strong>  <br/>  { description } </CardText>
+                <CardText expandable={true}> <strong>Description</strong>  <br/>  { description } </CardText>
                 {priority ? <CardText expandable={true} > {priority} priority </CardText> : null}
 
 
